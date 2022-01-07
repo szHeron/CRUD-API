657a1c5ef9e9f4701f156ec61ae0a1116039e606
szHeron <heronb7@gmail.com>
2022-01-07 15:57:47 -0300

    added status code and handle erros

--- api.py
+++ api.py
@@ -1,42 +1,67 @@
 from flask import Flask, request
 from dbmanager import addNewProduct, listProducts, delProduct
+import json
 
 app = Flask("Shop")
 
 @app.route("/products", methods=["GET"])
 def getProducts():
     try:
-        return {"products": listProducts()}
+        return {"products": listProducts()}, 200
     except Exception as e:
-        print(e)
-        return {"error": e}
+        response = app.get_response()
+        response.data = json.dumps({
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        })
+        response.content_type = "application/json"
+        return response
 
 @app.route('/products/<int:page_id>')
 def getSpecificProduct(page_id):
     try:
-        return {"product": listProducts(page_id)}
+        return {"product": listProducts(page_id)}, 200
     except Exception as e:
-        print(e)
-        return {"error": e}
+        response = app.get_response()
+        response.data = json.dumps({
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        })
+        response.content_type = "application/json"
+        return response
 
 @app.route("/addproduct", methods=["POST"])
 def newProduct():
     try:
         body = request.get_json()
         addNewProduct(body['name'], body['section'], body['price'], body['available'])
-        return {"success": True}
+        return 200
     except Exception as e:
-        print(e)
-        return {"success": False}
+        response = app.get_response()
+        response.data = json.dumps({
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        })
+        response.content_type = "application/json"
+        return response
 
 @app.route("/delproduct", methods=["DELETE"])
 def deleteProduct():
     try:
         body = request.get_json()
         delProduct(body['id'])
-        return {"success": True}
+        return 200
     except Exception as e:
-        print(e)
-        return {"success": False}
+        response = app.get_response()
+        response.data = json.dumps({
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        })
+        response.content_type = "application/json"
+        return response
 
 app.run()
