1b528ab85e01b43a968598c4e2338dfe6a68fc9a
szHeron <heronb7@gmail.com>
2022-01-07 15:46:56 -0300

    added delete and update in read

--- api.py
+++ api.py
@@ -1,11 +1,23 @@
 from flask import Flask, request
-from dbmanager import addNewProduct, listProducts
+from dbmanager import addNewProduct, listProducts, delProduct
 
 app = Flask("Shop")
 
 @app.route("/products", methods=["GET"])
 def getProducts():
-    return {"products": listProducts()}
+    try:
+        return {"products": listProducts()}
+    except Exception as e:
+        print(e)
+        return {"error": e}
+
+@app.route('/products/<int:page_id>')
+def getSpecificProduct(page_id):
+    try:
+        return {"product": listProducts(page_id)}
+    except Exception as e:
+        print(e)
+        return {"error": e}
 
 @app.route("/addproduct", methods=["POST"])
 def newProduct():
@@ -13,6 +25,18 @@ def newProduct():
         body = request.get_json()
         addNewProduct(body['name'], body['section'], body['price'], body['available'])
         return {"success": True}
-    except:
+    except Exception as e:
+        print(e)
         return {"success": False}
+
+@app.route("/delproduct", methods=["DELETE"])
+def deleteProduct():
+    try:
+        body = request.get_json()
+        delProduct(body['id'])
+        return {"success": True}
+    except Exception as e:
+        print(e)
+        return {"success": False}
+
 app.run()
--- dbmanager.py
+++ dbmanager.py
@@ -17,13 +17,24 @@ def checkExistsTable():
   cursor.execute("SELECT * FROM information_schema.tables WHERE table_name = 'products'")
   value = cursor.fetchall()
   if(len(value) < 1):
-    cursor.execute("CREATE TABLE products(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, section VARCHAR(255) NOT NULL, price DECIMAL(4,2) NOT NULL, available INT(4) DEFAULT 1)")
+    cursor.execute("CREATE TABLE products(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, section VARCHAR(255) NOT NULL, price DECIMAL(6,2) NOT NULL, available INT(4) DEFAULT 1)")
 
 def addNewProduct(name, section, price, available):
   cursor.execute("INSERT INTO products (name, section, price, available) VALUES(%s, %s, %s, %s)", (name, section, price, available))
   mydb.commit()
 
-def listProducts():
-  cursor.execute("SELECT JSON_OBJECT('id', id, 'name', name, 'section', section, 'price', price, 'available', available) FROM products")
-  products = cursor.fetchall()
-  return products
+def listProducts(id = -1):
+  if id > 0:
+    cursor.execute("SELECT JSON_OBJECT('id', id, 'name', name, 'section', section, 'price', price, 'available', available) FROM products WHERE id = {}".format(id))
+    return cursor.fetchone()
+  else:
+    cursor.execute("SELECT JSON_OBJECT('id', id, 'name', name, 'section', section, 'price', price, 'available', available) FROM products")
+    return cursor.fetchall()
+
+def delProduct(id):
+  cursor.execute("DELETE FROM products WHERE id = %s", (id))
+  mydb.commit()
+
+def updateProduct(id):
+  cursor.execute("UPDATE products SET name WHERE id = {}".format(id))
+  mydb.commit()
